1ec12165ac6296fcafaa7f33ece1b5d7a84d6450
Nick Dunn <nick@ncdunn.com>
2023-12-03 12:51:40 -0500

    Update sart-tp.py
    basic functionality

--- sart-tp.py
+++ sart-tp.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+from ast import withitem
 
 from psychopy import locale_setup
 from psychopy import prefs
@@ -12,6 +13,7 @@ from numpy import (sin, cos, tan, log, log10, pi, average,
 from numpy.random import random, randint, normal, shuffle, choice as randchoice
 import os  # handy system and path functions
 import sys  # to get file system encoding
+import pandas as pd
 
 from psychopy.hardware import keyboard
 
@@ -47,6 +49,89 @@ endExpNow = False  # flag for 'escape' or other condition => quit the exp
 frameTolerance = 0.001  # how close to onset before 'same' frame
 
 #-------------------EXPERIMENT CODE---------------------------------------------
+
+# Setup window
+win = visual.Window(
+    size=(2560, 1440), fullscr=True, screen=0,
+    winType='pyglet', allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True, units='deg')
+
+# Store monitor framerate
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0
+
+# Create default keyboard
+defaultKeyboard = keyboard.Keyboard()
+
+# Create default visual stimulus
+stim = visual.TextStim(win)
+
+# Initialize list of block condition files
+blockFiles = ['conditions.xlsx']
+
+
+
+# Initialize global clock
+globalClock = core.Clock()
+
+# Experiment loop
+for block in blockFiles:
+    trials = data.TrialHandler(nReps=1, method='sequential', trialList=data.importConditions(block))
+    timer = core.CountdownTimer()
+    #stimulusOffset = 0
+    for trial in trials:
+        trialClock = core.MonotonicClock()
+        startTime = trialClock.getTime() * 1000
+        logging.log(level=logging.EXP, msg=f"Trial start: {startTime}")
+
+        stimulusDuration = trial['stimulusDuration'] / 1000.0
+        isi = trial['isi'] / 1000.0
+        stimulusOnset = trial['stimulusOnset'] / 1000.0
+        #trialDuration = stimulusDuration + isi
+        #stimulusOffset += trialDuration
+        stimulus = trial['stimulus']
+
+        
+        timer.add(stimulusDuration)
+        while timer.getTime() > 0:
+           stim.setText(stimulus)
+           stim.draw()
+           win.flip()
+        
+        timer.add(isi)
+        
+        while timer.getTime() > 0:
+            win.flip()
+        
+
+        # Log the end time of the trial
+        endTime = trialClock.getTime() * 1000
+        logging.log(level=logging.EXP, msg=f"Trial end: {endTime}")
+        logging.log(level=logging.EXP, msg=f"Global Clock:{globalClock.getTime()}")
+        
+        if 'escape' in event.getKeys():
+            win.close()
+            core.quit()
+
+win.flip()
+
+thisExp.saveAsWideText(filename+'.csv', delim='auto')
+thisExp.saveAsPickle(filename)
+logging.flush()
+
+thisExp.abort()
+win.close()
+core.quit()
+        
+        
+
+
+
+
 
 
 
